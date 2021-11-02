fe05c523f1055498184c0363cb02cf067fd004ce
Sathiya31 <kspriyan31@gmail.com>
2021-11-02 23:09:13 +0530

    allow user to send function to extract key
    User can pass function to extract key value from their parameters
    Replaced nippy/freeze with encore/pr-edn

--- src/wrap/cache/redis.clj
+++ src/wrap/cache/redis.clj
@@ -2,44 +2,49 @@
   (:require [wrap.core :as w]
             [wrap.cache.core :as c]
             [wrap.tools :as t]
-            [taoensso.nippy :as nippy]
+            [taoensso.encore :as encore]
             [obiwan.core :as redis]))
 
-(defn- make-key [k args]
-  ;; (!) dropping the first arg by assuming it is a db/IO resources is
-  ;; a hack: needs more thinking
-  (let [without-db (rest args)] ;; <<< needs more thinking
-    (str k ":" (t/hash-it without-db))))
+(defn make-key [k key-fn args]
+  ;; use key-fn from user to extract the key value
+  ;; else hash all arguments and use as key
+
+  ;; dev=> (make-key "test" #(-> % last :foo) ["baz" {:foo "bar"}])
+  ;; "test:bar"
+  ;; dev=> (make-key "keys" first ["foo" "bar" "baz"])
+  ;; "keys:foo"
+    (let [key (if key-fn (key-fn args) (t/hash-it args))]
+      (str k " : " key)))
 
 ;; if more than redis place this ns behind a couple of protocols
 
-(defn lookup [conn prefix args]
+(defn lookup [conn prefix key-fn args]
   (when-let [v (redis/get conn
-                          (make-key prefix args))]
-    (nippy/thaw (.getBytes v))))
+                          (make-key prefix key-fn args))]
+    (encore/read-edn v)))
 
-(defn store [conn prefix args v]
+(defn store [conn prefix key-fn args v]
   (when (and v args)
     (redis/set conn
-               (make-key prefix args)
-               (String. (nippy/freeze v)))))
+               (make-key prefix key-fn args)
+               (encore/pr-edn v))))
 
-(defn delete [conn prefix args]
+(defn delete [conn prefix args key-fn]
   (when args
     (redis/del conn
-               [(make-key prefix args)])))
+               [(make-key prefix key-fn args)])))
 
 ;; wrappers
-(defn cache [conn prefix fs]
+(defn cache [conn prefix fs & [key-fn]]
   (w/wrap fs
-          (c/cache (partial lookup conn prefix)
-                   (partial store conn prefix))))
+          (c/cache (partial lookup conn prefix key-fn)
+                   (partial store conn prefix key-fn))))
 
-(defn evict [conn prefix fs]
+(defn evict [conn prefix fs & [key-fn]]
   (w/wrap fs
-          (c/evict (partial delete conn prefix))))
+          (c/evict (partial delete conn prefix key-fn))))
 
-(defn put [conn prefix fs]
+(defn put [conn prefix fs & [key-fn]]
   (w/wrap fs
-          (c/put (partial store conn prefix))))
+          (c/put (partial store conn prefix key-fn))))
 
